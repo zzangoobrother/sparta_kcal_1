40ad49432c714e279c1e1cb1cffdd21801ddcc0a
zzangoobrother <com1368@naver.com>
2021-09-15 13:17:12 +0900

    회원가입

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
 import math
@@ -66,6 +68,30 @@ def profile():
 def login():
     return render_template("login.html")
 
+# 회원가입페이지
+@app.route('/member/join')
+def member_join():
+    return render_template("join.html")
+
+@app.route('/member/check', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+@app.route('/mamber/join', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    nickname_receive = request.form['nickname_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        "nickname": nickname_receive,                               # 닉네임
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
 
 #오늘의프로필등록
 @app.route('/api/profile', methods=['POST'])
